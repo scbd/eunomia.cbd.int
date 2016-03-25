3821b8862aedaa1352cef9d44f4bbc65abfead41
Randy Houlahan <randy.houlahan@cbd.int>
2016-03-25 17:39:12 -0400

    index start

--- app/views/index.js
+++ app/views/index.js
@@ -1,2 +1,29 @@
-define([], function() {
-})
+define(['app', 'lodash','jquery',
+  'css!./index', '../services/mongo-storage',
+        'services/filters',
+        './menu'
+], function(app,_,$) {
+
+    app.controller("home", ['$scope','mainMenu', '$http','$filter','$route','mongoStorage','$location','$element','$timeout','$window','$anchorScroll','authentication', //"$http", "$filter", "Thesaurus",
+      function($scope,mainMenu,  $http,$filter,$route,mongoStorage,$location,$element,$timeout,$window,$anchorScroll,auth) { //, $http, $filter, Thesaurus
+
+
+        $scope.toggle = mainMenu.toggle;
+        $scope.sections = mainMenu.getMenu('mainMenu');
+          auth.getUser().then(function(user){
+
+             $scope.user = user;
+ //              console.log($scope.user);
+           }).then(function(){init();});
+
+          //=======================================================================
+          //
+          //=======================================================================
+          function init(){
+
+
+          }//init
+
+
+      }]);
+});
