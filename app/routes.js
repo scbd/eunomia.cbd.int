1f30a15fb79a492e9b34f559be3989535bc5b19b
Randy Houlahan <randy.houlahan@cbd.int>
2016-03-25 17:33:33 -0400

    update routes use services for main services

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,5 @@
-'use strict';
 
-define(['app','lodash' 'extended-route-provider'], function(app,_) {
+define(['app', 'lodash', 'text!views/index.html', 'views/index', 'scbd-angularjs-services/extended-route',  'scbd-angularjs-services/authentication'], function(app, _, rootTemplate) { 'use strict';
 
     app.config(['extendedRouteProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
@@ -8,50 +7,51 @@ define(['app','lodash' 'extended-route-provider'], function(app,_) {
         $locationProvider.hashPrefix('!');
 
         $routeProvider.
-            when('/',                   { templateUrl: '/app/views/index.html',                 resolveController: true, resolveUser: true }).
-            when('/about',              { templateUrl: '/app/views/about.html',                 resolveUser: true }).
-            when('/countries',          { templateUrl: '/app/views/samples/country-index.html', resolveController: true, resolveUser: true }).
-            when('/countries/:country', { templateUrl: '/app/views/samples/country.html',       resolveController: true, resolveUser: true }).
-            when('/case-studies/:id',   { templateUrl: '/app/views/samples/case-study.html',    resolveController: true, resolveUser: true }).
-            when('/404',                { templateUrl: '/app/views/404.html',                   resolveUser: true }).
+            when('/',                   { template:    rootTemplate,  label:'Home',  resolveController: 'views/index', reloadOnSearch : false }).
+            when('/home', { redirectTo: '/' }).
+            when('/',                     { templateUrl: 'views/index.html',                 resolveController: true, resolveUser: true }).
+            // when('/about',                { templateUrl: 'views/about.html',                 resolveUser: true }).
+            when('/help',                 { templateUrl: 'views/help.html',                  resolveUser: true }).
+
+            when('/admin',                    { templateUrl: 'views/admin/admin-dash-board.html',            resolveController: true, resolveUser: true}).
+            when('/admin/events',              { templateUrl: 'views/admin/events.html',                resolveController: true, resolveUser: true,resolve : { securized : securize(['Administrator','IndeAdministrator']) } }).
+            when('/admin/users',               { templateUrl: 'views/admin/users.html',                 resolveController: true, resolveUser: true,resolve : { securized : securize(['Administrator','IndeAdministrator']) } }).
+            when('/admin/organizations',       { templateUrl: 'views/admin/organizations.html',         resolveController: true, resolveUser: true,reloadOnSearch : false ,resolve : { securized : securize(['Administrator','IndeAdministrator']) } }).
+            when('/admin/config',              { templateUrl: 'views/admin/config.html',                resolveController: true, resolveUser: true, reloadOnSearch : false,resolve : { securized : securize(['Administrator','IndeAdministrator']) }  }).
+            when('/admin/meetings',            { templateUrl: 'views/admin/meetings.html',              resolveController: true, resolveUser: true, reloadOnSearch : false,resolve : { securized : securize(['Administrator','IndeAdministrator']) }  }).
+            when('/admin/meetings/:id',        { templateUrl: 'views/admin/edit-meetings.html',         resolveController: true, resolveUser: true, reloadOnSearch : false, resolve : { securized : securize(['Administrator','IndeAdministrator']) } }).
+
+
+            when('/schedule/day',             { templateUrl: 'views/schedule/day.html',                resolveController: true, resolveUser: true }).
+            when('/side-events',              { templateUrl: 'views/side-events.html',            resolveController: true, resolveUser: true }).
+
+            when('/404',                       { templateUrl: 'views/404.html',                          resolveUser: true }).
             otherwise({ redirectTo: '/404' });
     }]);
 
+
     //============================================================
-      //
-      //
-      //============================================================
-      function currentUser() {
+    //
+    //
+    //============================================================
+    function securize(roles) {
 
-          return ['authentication', function (authentication) {
+        return ['$location', '$window', '$q','authentication', function ($location, $window, $q, authentication) {
 
-              return authentication.getUser();
-          }];
-      }
+            return authentication.getUser().then(function (user) {
 
-      //============================================================
-      //
-      //
-      //============================================================
-      function securize(roles) {
+                if (!user.isAuthenticated) {
+                    var returnUrl = $window.encodeURIComponent($window.location.href);
+                    $window.location.href = 'https://accounts.cbd.int/signin?returnUrl=' + returnUrl; // force sign in
+                    return $q(function () {});
+                }
+                else if (roles && !_.isEmpty(roles) && _.isEmpty(_.intersection(roles, user.roles))) {
+                    $location.url('/help/403'); // not authorized
+                }
 
-          return ['$location', '$window', '$q', 'authentication', function ($location, $window, $q, authentication) {
+                return user;
+            });
+        }];
+    }
 
-              return authentication.getUser().then(function (user) {
-
-                  if (!user.isAuthenticated) {
-
-                      var returnUrl = $window.encodeURIComponent($window.location.href);
-                      $window.location.href = 'https://accounts.cbd.int/signin?returnUrl=' + returnUrl; // force sign in
-                      return $q(function () {});
-                  }
-                  else if (roles && !_.isEmpty(roles) && _.isEmpty(_.intersection(roles, user.roles))) {
-
-                      $location.url('/help/403'); // not authorized
-                  }
-
-                  return user;
-              });
-          }];
-      }
 });
