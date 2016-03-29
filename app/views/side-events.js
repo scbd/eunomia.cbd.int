e6dc3c9d11499a229c8e8c25f81ac9ba40362e33
Randy Houlahan <randy.houlahan@cbd.int>
2016-03-29 08:50:40 -0400

    movable rooms, side event styling, drag and drop functioning,everything else here https://trello.com/c/Mv6Vnai0

--- app/views/side-events.js
+++ app/views/side-events.js
@@ -7,10 +7,10 @@ define(['app', 'lodash','moment', 'BM-date-picker',
 
 ], function(app, _,moment) {
 
-  app.controller("events", ['$scope','$element','$document','dragulaService','mongoStorage',
-    function($scope,$element,$document,dragulaService,mongoStorage) {
+  app.controller("events", ['$scope','$element','$document','dragulaService','mongoStorage','$timeout','$rootScope',
+    function($scope,$element,$document,dragulaService,mongoStorage,$timeout,$rootScope) {
 
-$scope.many2=['ssssss','aaaaaaa','bbbbbbbbb','ccccccc','dddddddddd','eeeeeeee'];
+
 // $scope.startFilter=0;
 // $scope.endFilter=0;
 
@@ -24,8 +24,99 @@ init();
 //============================================================
 function init(){
     $scope.options={};
-    initMeeting().then(function(){generateDays();}).then(function(){loadRooms();});
+    initMeeting().then(function(){generateDays();}).then(function(){loadRooms();}).then(function(){initSideEvents($scope.meeting);});
+
 }//init
+//============================================================
+//
+//============================================================
+$scope.changeMeeting= function(){
+    generateDays();
+    loadRooms().then(function(){initSideEvents($scope.meeting);});
+
+};//init
+
+//============================================================
+//
+//============================================================
+function dateChangeEffect (id){
+    $element.find('#'+id).parent().addClass('is-focused');
+
+    $timeout(function(){
+      $element.find('#'+id).parent().removeClass('is-focused');
+    },2000);
+};//init
+
+//============================================================
+//
+//============================================================
+$scope.dateChange= function(id){
+    var dayTS;
+    var startDatTS = moment($scope.startDate).unix();
+    var endDatTS = moment($scope.endDate).unix();
+
+    if(id==='start-filter'){
+        $element.find('#end-filter').bootstrapMaterialDatePicker('setMinDate', $scope.startDate);
+          _.each($scope.days,function(day,key){
+                dayTS = moment(day.date).unix();
+                if(dayTS < startDatTS){
+                    day.selected=false;
+                    _.each($scope.options.rooms,function(room){
+                        room.bookings[key].selected=false;
+                    });
+                }else {
+                   day.selected=true;
+                   _.each($scope.options.rooms,function(room){
+                       room.bookings[key].selected=true;
+                   });
+                }
+          });
+    }
+    if(id==='end-filter'){
+        $element.find('#start-filter').bootstrapMaterialDatePicker('setMaxDate', $scope.endDate);
+          _.each($scope.days,function(day,key){
+                dayTS = moment(day.date).unix();
+                if(dayTS > endDatTS || dayTS < startDatTS){
+                    day.selected=false;
+                    _.each($scope.options.rooms,function(room){
+                        room.bookings[key].selected=false;
+                    });
+                }else {
+                   day.selected=true;
+                   _.each($scope.options.rooms,function(room){
+                       room.bookings[key].selected=true;
+                   });
+                }
+          });
+    }
+    dateChangeEffect(id);
+};//init
+//============================================================
+//
+//============================================================
+function initSideEvents(meeting){
+    return mongoStorage.loadUnscheduledSideEvents (meeting).then(function(res){
+      $scope.sideEvents= res.data;
+
+    }).then(
+          function(){
+        _.each($scope.sideEvents,function(res){
+            mongoStorage.loadDoc('inde-side-events',res.link._id).then(
+              function(se){
+                res.sideEvent=se;
+
+                res.sideEvent.orgs = [];
+                _.each(res.sideEvent.hostOrgs, function(org) {
+                  mongoStorage.loadDoc('inde-orgs', org).then(function(conf) {
+                    res.sideEvent.orgs.push(conf);
+                  });
+                    });
+              }
+            );//load doc
+        });// each
+      }
+    );//.then(function(){console.log($scope.sideEvents)})
+}//initMeeting
 
 //============================================================
 //
@@ -47,22 +138,41 @@ function initMeeting(){
       $scope.options.confrences[selectedKey].selected=true;
       $scope.meeting=$scope.options.confrences[selectedKey]._id;
     });
-}//generateDays
+}//initMeeting
 
 //============================================================
 //
 //============================================================
 function generateDays(){
+  $scope.days=[];
   var meeting = _.findWhere($scope.options.confrences,{_id:$scope.meeting});
+
+
   var numDays = Math.round((Number(meeting.end)-Number(meeting.start))/(24*60*60));
   var seconds = Number(meeting.start);
   var date =moment.unix(seconds);
+  console.log('meeting start',moment.unix(meeting.start).format("MMM DD"));
+  console.log('meeting end',moment.unix(meeting.end).format("MMM DD"));
 
+  $element.find('#start-filter').bootstrapMaterialDatePicker('setDate', date);
+  $element.find('#start-filter').bootstrapMaterialDatePicker('setMinDate', date);
+  $element.find('#end-filter').bootstrapMaterialDatePicker('setMinDate', date);
   for (var i = 1; i <= numDays+1; i++) {
-        $scope.days.push({'date':seconds,'month':date.format("MMM").toUpperCase(),'day':date.format("DD"),'lunch':['0'],'afternoon':['0']});
+        if(i===numDays+1){
+            $element.find('#end-filter').bootstrapMaterialDatePicker('setMaxDate', date);
+            $element.find('#start-filter').bootstrapMaterialDatePicker('setMaxDate', date);
+            $element.find('#end-filter').bootstrapMaterialDatePicker('setDate', date);
+        }
+
+        $scope.days.push({'selected':true,'date':date.format("YYYY-MM-DD"),'month':date.format("MMM").toUpperCase(),'day':date.format("DD"),'lunch':[],'afternoon':[{'title':'sssssss'}]});
         seconds = seconds + (24*60*60);
         date = moment.unix(seconds);
   }
+  $timeout(function(){
+    dateChangeEffect('start-filter');
+    dateChangeEffect('end-filter');
+  },1500);
+
 }//generateDays
 
 //============================================================
@@ -79,41 +189,78 @@ function loadRooms(){
 
 }//generateDays
 
-$scope.initSingleBage = function (el){
+$scope.initSingleBag = function (el){
 
 
 }
 
-
+dragulaService.options($scope, 'se-bag', {
+  mirrorAnchor: 'bottom'
+});
 //dragula test code
 //$('#test').bootstrapMaterialDatePicker('setDate', moment());
 dragulaService.options($scope, 'venue-bag', {
   moves: function (el, container, handle) {
     return handle.className === 'grabbible room-title ng-binding';
-  }
+  },
+
 });
 $scope.$on('se-bag.drag', function (e, el,container) {
   //container.removeClass('grabbible');
   //el.addClass('gu-mirror');
-console.log(container);
+//   var test =angular.element("<div>test</div>")
+//   el=test;
+// console.log(el);
 });
+$scope.$on('se-bag.cloned', function (e, mirror,shadow) {
 
+mirror.children('div.panel.panel-default.se-panel').hide();
+mirror.children('div.drag-view.text-center').show();
+shadow.children('div.panel.panel-default.se-panel').hide();
+shadow.children('div.drag-view.text-center').show();
+
+mirror.height(20);
+mirror.width(80);
+shadow.height(20);
+shadow.width(80);
+
+});
+$scope.$on('se-bag.shadow', function (e, el, container,source) {
+
+    if( container[0].id==='unscheduled-side-events'){
+      el.children('div.panel.panel-default.se-panel').show();
+      el.children('div.drag-view.text-center').hide();
+      el.height(185);
+      el.width(260);
+    }else{
+      el.children('div.panel.panel-default.se-panel').hide();
+      el.children('div.drag-view.text-center').show();
+      el.height(20);
+      el.width(80);
+    }
+
+});
 $scope.$on('se-bag.drop', function (e, el,container) {
-  el.addClass('ex-moved');
-  console.log(container);
+  //if source revert container
+  //el.addClass('ex-moved');
+ $rootScope.$broadcast("showInfo","Test info Title", "Test info message");
+ // $rootScope.$broadcast("showWarning","Test warning title", "Test warning message");
+ // $rootScope.$broadcast("showSuccess","Test info Title", "Test info message");
+ // $rootScope.$broadcast("showError","Test warning title", "Test warning message");
 });
+//
+ $scope.$on('se-bag.over', function (e, el, container) {
 
-$scope.$on('third-bag.over', function (e, el, container) {
-  container.addClass('ex-test');
 
-});
+ });
+ $scope.$on('se-bag.drop-model', function (el, target, source) {
 
-$scope.$on('third-bag.out', function (e, el, container) {
-  container.removeClass('ex-test');
-});
-dragulaService.options($scope, 'third-bag', {
-  removeOnSpill: true
-});
+
+ });
+// $scope.$on('third-bag.out', function (e, el, container) {
+//   container.removeClass('ex-test');
+// });
+
 $document.ready(function(){
   $.material.init();
   $.material.input();
