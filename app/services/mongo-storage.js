3302aadcd655ebd2d59773627b023a3a20958da6
Randy Houlahan <randy.houlahan@cbd.int>
2016-03-29 08:49:41 -0400

    remove unused function from storage service add in new ones

--- app/services/mongo-storage.js
+++ app/services/mongo-storage.js
@@ -5,56 +5,12 @@ app.factory("mongoStorage", ['$http','authentication','$q','locale','$location',
         var user;
         authentication.getUser().then(function(u){
           user=u;
-          // if( _.intersection(['Administrator','IndeAdministrator'], user.roles).length>0)
-          // {
-          //   deleteTempRecords('inde-orgs');
-          //   deleteTempRecords('inde-side-events');
-          // }
 
         });
         var clientOrg = 0; // means cbd
 
         var statuses=['draft','published','request','deleted','archived','canceled','rejected'];
 
-
-        //============================================================
-        //
-        //============================================================
-        function save (schema,document,_id){
-               var url        = '/api/v2015/'+schema;
-               var params     = {};
-               var data       = (document||{});
-
-                if(_id){
-                    params.id = _id;
-                    url=url+'/'+_id;
-                    data.clientOrg = clientOrg;
-                    return touch(data).then(function(){return $http.put(url,data,params);});
-
-                }
-                else{
-                    data.clientOrg=clientOrg;
-                    return touch(data).then(function(){return $http.post(url,data,params);});
-                }  //create
-        }
-
-
-        // //============================================================
-        // //
-        // //============================================================
-        // function deleteTempRecords(schema) {
-        //
-        //     var params = {
-        //                     q:{'meta.v':0},
-        //                     cache:false
-        //                   };
-        //     $http.get('/api/v2015/'+schema,{'params':params}).then(function(res){
-        //           _.each(res.data,function(obj){
-        //                 $http.delete('/api/v2015/'+schema+'/'+obj._id);
-        //           });
-        //     });
-        //
-        // }
 
         //============================================================
         //
@@ -75,19 +31,7 @@ app.factory("mongoStorage", ['$http','authentication','$q','locale','$location',
                         }
                   );
         }
-        //============================================================
-        //
-        //============================================================
-        function loadArchives (schema){
-          //+'?q={"_id":{"$oid":"'+_id+'"},"clientOrganization":'+clientOrg+'}&f={"document":1}'
-            if(!schema) throw "Error: failed to indicate schema loadArchives";
-            var params = {
-                          q:{'meta.status':'archived'},
 
-                        };
-            return $q.when( $http.get('/api/v2015/'+schema,{'params':params}));
-
-        }
         //============================================================
         //
         //============================================================
@@ -126,41 +70,7 @@ app.factory("mongoStorage", ['$http','authentication','$q','locale','$location',
 
 
         }
-        //============================================================
-        //
-        //============================================================
-        function loadOwnerDocs (schema){
 
-            if(!schema) throw "Error: failed to indicate schema loadDocs";
-            return  $q.when( authentication.getUser().then(function(u){
-                      user=u;
-                    }).then( function(){
-                      var params = {
-                                  q:{'meta.status':{$nin:['archived','deleted']},
-                                     'meta.createdBy':user.userID,
-                                     'meta.v':{$ne:0}
-                                    }
-                                };
-                        return $http.get('/api/v2015/'+schema,{'params':params});
-                      }));
-        }
-        //=======================================================================
-        // creates a doc with version  0 in order to have a base doc for images
-        //
-        //=======================================================================
-        function createDoc (schema){
-              var obj = {
-                      meta:{
-                        locales:[_.clone(locale)],
-                        v:-1,
-                        status: 'draft'
-                      }
-                  };
-
-              return save(schema,obj).then(function(res){
-                  return loadDoc (schema,res.data.id);
-              });
-        }
 
         //=======================================================================
         //
@@ -248,56 +158,7 @@ app.factory("mongoStorage", ['$http','authentication','$q','locale','$location',
 
               });
         }//getStatusFacits
-        //=======================================================================
-        //
-        //=======================================================================
-        function getOwnerFacits(schema,statusFacits,statArry,stat){
-          statusFacits.draft=0;
-          statusFacits.deleted=0;
-          statusFacits.approved=0;
-          statusFacits.rejected=0;
-          statusFacits.canceled=0;
-          statusFacits.archived=0;
-          statusFacits.all=0;
-          return  $q.when( authentication.getUser().then(function(u){
-                            user=u;
-                          }).then( function(){
-                            statusFacits.all=0;
-                            if(!statArry)
-                              statArry=statuses;
-                            if(stat){
-                              $http.get('/api/v2015/'+schema+'?c=1&q={"meta.status":"'+stat+'","meta.v":{"$ne":0},"meta.createdBy":'+user.userID+'}').then(
-                                function(res){
 
-                                  statusFacits[stat]=res.data.count;
-                                  statusFacits['all']+=res.data.count;
-                                }
-                              );
-                            }
-                            else
-                            _.each(statArry,function(status){
-
-                                  $http.get('/api/v2015/'+schema+'?c=1&q={"meta.status":"'+status+'","meta.v":{"$ne":0},"meta.createdBy":'+user.userID+'}').then(
-                                    function(res){
-                                      statusFacits[status]=res.data.count;
-                                      statusFacits['all']+=res.data.count;
-                                    }
-                                  );
-                            });
-                          })
-                  );
-        }//getStatusFacits
-        //=======================================================================
-        //
-        //=======================================================================
-        function generateEventId(){
-              var params = {};
-              params.f={'id':1};
-              params.s={ 'id' : -1 };
-              params.l=1;
-              return  $http.get('/api/v2015/inde-side-events',{'params':params});
-
-        }//getStatusFacits
 
         //============================================================
         //
@@ -318,7 +179,25 @@ app.factory("mongoStorage", ['$http','authentication','$q','locale','$location',
             }
 
         }
+        //============================================================
+        //
+        //============================================================
+        function loadUnscheduledSideEvents (meeting){
 
+            var params={};
+
+
+              params = {
+                          q:{'link.meeting':meeting,
+                              'start':{'$exists':0}
+                            }
+
+
+                        };
+              return $http.get('/api/v2016/reservations',{'params':params});
+
+
+        }
         //============================================================
         //
         //============================================================
@@ -338,83 +217,25 @@ app.factory("mongoStorage", ['$http','authentication','$q','locale','$location',
             }
 
         }
-        //=======================================================================
-        //
-        //=======================================================================
-        function touch(doc){
-          return authentication.getUser().then(function(u){
-            user=u;
 
-              if(!user.userID) throw "Error no userID to touch record";
-              if(!doc.meta) throw "Error mongo document contains no meta data";
 
-              if(!doc.meta.status) doc.meta.status='draft';
 
-              if(!doc.meta.createdBy && !doc.meta.createdOn){
-                doc.meta.createdBy = doc.meta.modifiedBy = user.userID;
-                doc.meta.createdOn  = doc.meta.modifiedOn = Date.now();
-              }else if (doc.meta.createdBy && doc.meta.createdOn){
-                doc.meta.modifiedBy = user.userID;
-                doc.meta.modifiedOn = Date.now();
-              }
-              doc.meta.v=Number(doc.meta.v)+1;
-
-              doc.meta.hash=sha256(JSON.stringify(doc));  //jshint ignore:line
-          });
-        } // touch
-
-        //=======================================================================
-        //
-        //=======================================================================
-        function uploadDocAtt(schema,_id,file){
-              if(!schema)throw "Error: no schema set to upload attachment";
-              if(!_id)throw "Error: no docId set to upload attachment";
-
-              var postData = {
-                filename: file.name,
-                //amazon messes with camel case and returns objects with hyphen in property name in accessible in JS
-                // hence no camalized and no hyphanized meta names
-                metadata:{
-                    createdby:user.userID,
-                    createdon:Date.now(),
-                    schema:schema,
-                    docid:_id,
-                    filename:file.name,
-                }
-              };
-              return $http.post('/api/v2015/temporary-files', postData).then(function(res) {
-              // Create a temp file location to upload to
-                return res.data;
-              }).then(function(target) {
-                  // upload file to temp area
-                  return $http.put(target.url, file, {
-                    headers: {
-                      'Content-Type': target.contentType
-                    }
-                  }).then(function() {
-                    // move temp file form temp to its proper home schema/is/filename
-                    return $http.get("/api/v2015/mongo-document-attachment/"+target.uid, { });
-                  });
-              });
-        } // touch
 
         return{
           loadRooms:loadRooms,
-          getOwnerFacits:getOwnerFacits,
+
           requestDoc:requestDoc,
           rejectDoc:rejectDoc,
           approveDoc:approveDoc,
           cancelDoc:cancelDoc,
-          generateEventId:generateEventId,
+
           getStatusFacits:getStatusFacits,
           deleteDoc:deleteDoc,
           loadDoc:loadDoc,
-          loadOwnerDocs:loadOwnerDocs,
-          createDoc:createDoc,
-          save:save,
-          uploadDocAtt:uploadDocAtt,
+
+loadUnscheduledSideEvents:loadUnscheduledSideEvents,
           archiveDoc:archiveDoc,
-          loadArchives:loadArchives,
+
           loadDocs:loadDocs,
           loadConfrences :loadConfrences ,
           unArchiveDoc:unArchiveDoc
