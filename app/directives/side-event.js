e7533f73779392ff943404d9c012554f575f3755
Randy Houlahan <randy.houlahan@cbd.int>
2016-04-08 08:14:21 -0400

    show title on hover

--- app/directives/side-event.js
+++ app/directives/side-event.js
@@ -16,6 +16,18 @@ define(['app',
 
 
         controller: function($scope, $element) {
+          var orgs = $element.find("#title").popover({ placement: 'top', html: 'true',container: 'body',
+                 content: function() {
+                   return $element.find('#pop-title').html();
+                 }
+           });
+           var titleEl = $element.find("#title");
+           titleEl.on('mouseenter', function() {
+                 orgs.popover('show');
+           });
+           titleEl.on('mouseleave', function() {
+                 orgs.popover('hide');
+           });
           $timeout(function(){
             if($scope.res.sideEvent && $scope.res.sideEvent.orgs.length >1 ){
                 var orgEl = $element.find("div.num-orgs");//.popover({ placement: 'bottom', html: 'true'});
