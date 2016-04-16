d87c6419e148aa6044abeaad6e08e0f8f2eb374a
Randy Houlahan <randy.houlahan@cbd.int>
2016-04-16 19:32:12 -0400

    update to day schedule

--- app/views/schedule/day.js
+++ app/views/schedule/day.js
@@ -1,9 +1,47 @@
 define(['app', 'lodash',
-
+'BM-date-picker',
+  'ngDialog',
+  '../../services/mongo-storage',
+  '../../directives/forms/edit/room',
 ], function(app, _) {
 
-  app.controller("organizations", ['$scope',
-    function($scope) { //, $http, $filter, Thesaurus
+  app.controller('day',['$scope','$document','$element','scbdMenuService',
+    function($scope,$document,$element,scbdMenuService) {
 
+
+      init();
+
+      //============================================================
+      //
+      //============================================================
+      function init() {
+        $scope.options = {};
+        $scope.toggle = scbdMenuService.toggle;
+
+      } //init
+
+      //============================================================
+      //
+      //============================================================
+      $document.ready(function() {
+
+        $.material.init();
+        $.material.input();
+        $.material.ripples();
+
+        $element.find('#day').bootstrapMaterialDatePicker({
+          weekStart: 0,
+          time: false
+        });
+        $element.find('#end-filter').bootstrapMaterialDatePicker({
+          weekStart: 0,
+          time: false
+        });
+        $element.find('#start-filter').bootstrapMaterialDatePicker({
+          weekStart: 0,
+          time: false
+        });
+
+      });
   }]);
 });
