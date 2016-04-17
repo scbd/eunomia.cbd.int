56022d855e67cd3ef5e0e3634131f90ffeffc034
Randy Houlahan <randy.houlahan@cbd.int>
2016-04-17 17:48:31 -0400

    room row directive

---
+++ app/directives/schedule/room-row.js
@@ -0,0 +1,46 @@
+define(['app', 'lodash', 'text!./room-row.html','text!../forms/edit/room-dialog.html','css!./room-row.css',
+  'ngDialog',
+  '../forms/edit/room',
+], function(app, _, template,roomDialog) {
+
+  app.directive("roomRow", ['ngDialog',
+    function(ngDialog) {
+      return {
+        restrict: 'E',
+        template: template,
+        replace: true,
+        transclude: false,
+        scope: {
+          'room': '='
+        },
+        controller: function($scope,scheduleService) {
+
+            init();
+
+            //============================================================
+            //
+            //============================================================
+            function init() {
+                $scope.rowHeight=scheduleService.getRowHeight();
+
+            } //init
+
+            //============================================================
+            //
+            //============================================================
+            $scope.roomDialog = function(room) {
+                $scope.editRoom = room;
+                ngDialog.open({
+                  template: roomDialog,
+                  className: 'ngdialog-theme-default',
+                  closeByDocument: true,
+                  plain: true,
+                  scope: $scope
+                });
+            };//$scope.roomDialog
+
+          } //link
+      }; //return
+    }
+  ]);
+});
