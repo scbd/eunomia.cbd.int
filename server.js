7d5f3ff93eaaabb1d2b785fdfb8b5eecfd1e99c9
Stephane Bilodeau <bilodeaustephane@gmail.com>
2016-04-18 16:59:55 -0400

    Code cleanup

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/* jshint node: true, browser: false */
+/* jshint node: true, browser: false, esnext: true */
 'use strict';
 
 process.on('SIGTERM', ()=>process.exit());
@@ -6,33 +6,28 @@ process.on('SIGTERM', ()=>process.exit());
 // CREATE HTTP SERVER AND PROXY
 
 var app = require('express')();
-var proxy   = require('http-proxy').createProxyServer({});
+var proxy = require('http-proxy').createProxyServer({});
 
 app.set('views', __dirname + '/app');
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
 
-// LOAD CONFIGURATION
-
-app.set('port', process.env.PORT || 2020);
-
-// CONFIGURE /APP/* ROUTES
+// CONFIGURE ROUTES
 
 app.use('/app',   require('serve-static')(__dirname + '/app_build'));
 app.use('/app',   require('serve-static')(__dirname + '/app'));
-app.all('/api/*',       function(req, res) { proxy.web(req, res, { target: 'https://api.cbddev.xyz', changeOrigin: true } ); } );
-app.all('/socket.io/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbddev.xyz', changeOrigin: true } ); } );
-
-app.all('/app/*', function(req, res) { res.status(404).send(); } );
+app.all('/app/*',       (req, res) => res.status(404).send());
+app.all('/api/*',       (req, res) => proxy.web(req, res, { target: 'https://api.cbddev.xyz', changeOrigin: true } ));
+app.all('/socket.io/*', (req, res) => proxy.web(req, res, { target: 'https://api.cbddev.xyz', changeOrigin: true } ));
 
 // CONFIGURE TEMPLATE
 
-app.get('/*', function (req, res) { res.render('template', { baseUrl: req.headers.base_url || '/' }); });
+app.get('/*', (req, res) => res.render('template', { baseUrl: req.headers.base_url || '/' }));
 
 // START SERVER
 
-app.listen(app.get('port'), function () {
+app.listen(process.env.PORT || 2020, function () {
 	console.log('Server listening on %j', this.address());
 });
 
