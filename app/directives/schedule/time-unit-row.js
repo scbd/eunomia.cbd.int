28949de1390ef9d86f5347c9755e8566635c080f
Randy Houlahan <randy.houlahan@cbd.int>
2016-04-19 04:54:04 -0400

    fixing timing issue and in loading time intervals and ad res edit form dialog

--- app/directives/schedule/time-unit-row.js
+++ app/directives/schedule/time-unit-row.js
@@ -1,37 +1,50 @@
-define(['app', 'lodash', 'text!./time-unit-row.html','css!./time-unit-row.css'
-], function(app, _, template) {
+define(['app', 'lodash', 'text!./time-unit-row.html','text!../forms/edit/reservation-dialog.html','css!./time-unit-row.css',  'ngDialog','../forms/edit/reservation',
+], function(app, _, template,resDialog) {
 
-  app.directive("timeUnitRow", [
-    function() {
+  app.directive("timeUnitRow", ['ngDialog',
+    function(ngDialog) {
       return {
         restrict: 'E',
         template: template,
         replace: true,
         transclude: false,
         scope: {
-          'doc': '='
+          'day': '=',
+          'startTime':'=',
+          'endTime':'=',
+          'conferenceDays':'=?'
         },
         controller: function($scope, $element,scheduleService) {
-
+            $scope.timeIntervals=[];
             init();
 
             //============================================================
             //
             //============================================================
             function init() {
+              var timeoutCount=0;
+
               var cancel = setInterval(function(){
 
                  $scope.rowHeight=Number(scheduleService.getRowHeight())+1;// +1 hack adoodle doo
                  $element.height($scope.rowHeight);
-                 $scope.outerRowWidth=scheduleService.getOuterGridWidth();
-                 $scope.timeIntervals=scheduleService.getTimeIntervals($scope.startTime,$scope.endTime);
+                 $scope.outerRowWidth=Number(scheduleService.getOuterGridWidth())*$scope.conferenceDays.length;
+
+                 if($scope.startTime && $scope.endTime)
+                    $scope.timeIntervals=scheduleService.getTimeIntervalsConference($scope.startTime,$scope.endTime);
 
                  $scope.hourOn=hourOn;
                  $scope.toHour=toHour;
-                 if($scope.rowHeight && $scope.outerRowWidth && $scope.timeIntervals){
+                 if($scope.rowHeight && $scope.outerRowWidth && !_.isEmpty($scope.timeIntervals)){
                      clearInterval(cancel);
                      calcColWidths();
                  }
+                 if(timeoutCount===20){
+
+                   clearInterval(cancel);
+                   throw "Error: initialization onf grid rows timed out 2 seconds";
+                 }
+                 timeoutCount++;
               },100);
 
             } //init
@@ -60,6 +73,20 @@ $scope.test=function(sub){alert(sub);};
 
                 return moment(val).format();
             } //init
+
+            //============================================================
+            //
+            //============================================================
+            $scope.resDialog = function(res) {
+                $scope.editRes = res;
+                ngDialog.open({
+                  template: resDialog,
+                  className: 'ngdialog-theme-default',
+                  closeByDocument: true,
+                  plain: true,
+                  scope: $scope
+                });
+            };//$scope.roomDialog
 
           } //link
       }; //return
