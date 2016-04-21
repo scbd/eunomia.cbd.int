944ab9573c8b322f9b49e3f0725ef3e7d7a8d3b8
Randy Houlahan <randy.houlahan@cbd.int>
2016-04-21 16:17:29 -0400

    restructure

--- app/directives/schedule/time-unit-row-header.js
+++ app/directives/schedule/time-unit-row-header.js
@@ -12,67 +12,93 @@ define(['app', 'lodash', 'text!./time-unit-row-header.html','moment','css!./time
           'day': '=',
           'startTime':'=',
           'endTime':'=',
-          'conferenceDays':'=?'
+          'conferenceDays':'='
         },
-        controller: function($scope,$element, scheduleService) {
+        controller: function($scope,$element,$document) {
 
-            init();
-//todo set start and end in service when set
+            $scope.$watch('conferenceDays',function(){
+              initTimeIntervals();
+            });
+            $scope.$watch('startTime',function(){
+
+              initTimeIntervals();
+            });
+            $scope.$watch('endTime',function(){
+
+              initTimeIntervals();
+            });
+
             //============================================================
             //
             //============================================================
-            function init() {
-              var timeOut=0;
+            function initTimeIntervals(){
+              if($scope.startTime.hours() && $scope.endTime.hours() && $scope.conferenceDays && !_.isEmpty($scope.conferenceDays)){
+                  var hours = $scope.endTime.hours()-$scope.startTime.hours();
 
-              // if($scope.conferenceDays && !_.isEmpty($scope.conferenceDays)){
-                     var cancel = setInterval(function(){
+                  $scope.timeIntervals = [];
+                  var t = moment($scope.conferenceDays[0]).add($scope.startTime.hours(),'hours').add($scope.startTime.minutes(),'minutes');
+                  for(var  i=0; i< hours ; i++)
+                  {
+                    $scope.timeIntervals.push(moment(t));
+                    t=t.add(1,'hours');
+                  }
+                  initOuterGridWidth();
 
-                        $scope.rowHeight=scheduleService.getHeadersHeight();
-                        if($scope.conferenceDays)
-                          $scope.outerRowWidth=scheduleService.getOuterGridWidth();
+              }
+            }//initTimeIntervals
 
-                        if($scope.startTime && $scope.endTime)
-                            $scope.timeIntervals=scheduleService.getTimeIntervalsHeader($scope.startTime,$scope.endTime);
-                        $element.height($scope.rowHeight);
-                        $scope.hourOn=hourOn;
-                        $scope.toHour=toHour;
-                        if($scope.rowHeight && $scope.outerRowWidth && $scope.timeIntervals && $scope.conferenceDays && !_.isEmpty($scope.conferenceDays)){
-                            clearInterval(cancel);
-                            calcColWidths();
-                        }
-                        if(timeOut===20){
-                            clearInterval(cancel);
-                            throw 'Error: unable to get time intervals, timeout 2 seconds';
-                        }
-                        timeOut++;
-                     },100);
-              //}
-            } //init
+            //============================================================
+            //
+            //============================================================
+            function initOuterGridWidth() {
+              var scrollGridEl;
+                    $document.ready(function(){
+                      $timeout(function(){
+                        scrollGridEl=$document.find('#scroll-grid');
+
+                        $scope.outerGridWidth=Number(scrollGridEl.width());
+                        initDayWidth();
+                        calcColWidths();
+
+                      });
+                    });
+
+            }//initOuterGridWidth
+
+            //============================================================
+            //
+            //============================================================
+            function initDayWidth(){
+                  _.each($scope.conferenceDays,function(con,key){
+                        $timeout(function(){$element.find('#day-header-'+key).width($scope.outerGridWidth);});
+
+                  });
+                  _.each($scope.conferenceDays,function(con,key){
+                        $timeout(function(){$element.find('#interval-header-'+key).width($scope.outerGridWidth);});
+
+                  });
+            }//initDayWidth
+
+            //============================================================
+            //
+            //============================================================
+            function initIntervalWidth(){
+                  _.each($scope.timeIntervals,function(con,key){
+                        $timeout(function(){$element.find('#sub-interval-header-'+key).width($scope.colWidth);});
+
+                  });
+            }//initIntervalWidth
 
             //============================================================
             //
             //============================================================
             function calcColWidths() {
 
-                $scope.colWidth = Number($scope.outerRowWidth)/Number($scope.timeIntervals.length);
-
+                $scope.colWidth = Number($scope.outerGridWidth)/Number($scope.timeIntervals.length);
+                initIntervalWidth();
             } //init
 
-            //============================================================
-            //
-            //============================================================
-            function hourOn(val) {
-
-                return ((Math.floor(val/4)%2)===0);
-            } //init
-            //============================================================
-            //
-            //============================================================
-            function toHour(val) {
-
-                return moment(val).format();
-            } //init
-          } //link
+          } //calcColWidths
       }; //return
     }
   ]);
@@ -81,17 +107,18 @@ define(['app', 'lodash', 'text!./time-unit-row-header.html','moment','css!./time
           return input.format('dddd [the] Do of MMMM, YYYY');
       };
   });
-  app.filter('floor', function() {
-      return function(input) {
-          return Math.floor(input);
-      };
-  });
+  // app.filter('floor', function() {
+  //     return function(input) {
+  //         return Math.floor(input);
+  //     };
+  // });
   app.filter('toHour', function() {
       return function(input) {
-          if(input.minutes()===0)
+          //if(input.minutes()===0)
+
             return input.format('hh:mm a');
-          else
-            return ' ';
+          // else
+          //   return ' ';
 
       };
   });
