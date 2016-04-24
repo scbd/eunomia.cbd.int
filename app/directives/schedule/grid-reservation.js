3e89b3c0c5330617c4873b7a236474ccb339e65a
Randy Houlahan <randy.houlahan@cbd.int>
2016-04-24 11:21:36 -0400

    ng-style for width timing issue

--- app/directives/schedule/grid-reservation.js
+++ app/directives/schedule/grid-reservation.js
@@ -44,23 +44,9 @@ console.log($scope.doc.resWidth);
                   return $element.find('#pop-title').html();
                 }
               });
-              var count = 0;
 
-              if (!$scope.doc.resWidth){
-                var cancelMinHeight = setInterval(function() {
-                  count++;
-                  if (count===10){ clearInterval(cancelMinHeight);
-                      throw "run away set interval in grid reservation";
-                  }
-                  if (!$scope.doc) clearInterval(cancelMinHeight);
-                  if ($scope.doc.resWidth) {
-                      $element.css('max-width',$scope.doc.resWidth+'px');
-                      clearInterval(cancelMinHeight);
-                  }
-                }, 500);
-              }else{
-                $element.css('max-width',$scope.doc.resWidth+'px');
-              }
+              if (!$scope.doc.resWidth)
+               $element.css('max-width',$scope.doc.resWidth+'px');
 
               titleEl.on('mouseenter', function() {
                 titleEl.popover('show');
@@ -70,16 +56,7 @@ console.log($scope.doc.resWidth);
               });
 
             } //triggerChanges
-            //============================================================
-            //
-            //============================================================
-            function calcResWidth (res) {
-                  var resStart = moment.utc(res.start).format('X');
-                  var resEnd = moment.utc(res.end).format('X');
 
-                  var resWidth = Math.ceil((resEnd - resStart)/timeUnit)*($scope.colWidth/subIntervals);
-                  return Number(resWidth);
-            };//calcResWidth
           } //link
       }; //return
     }
