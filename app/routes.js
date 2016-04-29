3bbc1e88532c3a5aefde647dafa2bad9c3f2ea59
Randy Houlahan <randy.houlahan@cbd.int>
2016-04-29 09:18:31 -0400

    refactor toast into file

--- app/routes.js
+++ app/routes.js
@@ -6,22 +6,21 @@ define(['app', 'lodash', 'services/extended-route',  'services/authentication'],
         $locationProvider.hashPrefix('!');
 
         $routeProvider.
-            when('/',                         { templateUrl: 'views/schedule/conference.html',                resolveController: true, resolveUser: true, resolve : { securized : securize(['Administrator','EunoAdministrator']) } }).
-            when('/home',                     { redirectTo: '/' }).
-            when('/schedule/location',        { templateUrl: 'views/schedule/location.html',           resolveController: true, resolveUser: true, resolve : { securized : securize(['Administrator','EunoAdministrator']) } }).
-            when('/schedule/conference',      { templateUrl: 'views/schedule/conference.html',         resolveController: true, resolveUser: true, resolve : { securized : securize(['Administrator','EunoAdministrator']) } }).
-            when('/side-events',              { templateUrl: 'views/side-events.html',                 resolveController: true, resolveUser: true, resolve : { securized : securize(['Administrator','EunoAdministrator']) } }).
-            when('/admin/reservation/types',  { templateUrl: 'views/admin/reservation-types.html',     resolveController: true, resolveUser: true, resolve : { securized : securize(['Administrator','EunoAdministrator']) } }).
+            when('/',                      { redirectTo: '/schedule/conference' }).
+            when('/schedule/conference',   { templateUrl: 'views/schedule/conference.html',  resolveController: true,  resolve : { securized : securize(['Administrator','EunoAdministrator']) }, menu:'schedule'}).
+            when('/schedule/side-events',  { templateUrl: 'views/schedule/side-events.html', resolveController: true,  resolve : { securized : securize(['Administrator','EunoAdministrator']) }, menu:'side-events'}).
 
-            when('/events/:eventId/cctv/feeds',      { templateUrl: 'views/cctv/feeds.html' ,   controllerAs:"feedsCtrl",   resolveController: true, resolve : { user : securize(['Administrator','EunoAdministrator']) } }).
-            when('/events/:eventId/cctv/feeds/:id',  { templateUrl: 'views/cctv/feed-id.html' , controllerAs:"feedIdCtrl",  resolveController: true, resolve : { user : securize(['Administrator','EunoAdministrator']) } }).
-            when('/events/:eventId/cctv/frames',     { templateUrl: 'views/cctv/frames.html',   controllerAs:"framesCtrl",  resolveController: true, resolve : { user : securize(['Administrator','EunoAdministrator']) } }).
-            when('/events/:eventId/cctv/frames/:id', { templateUrl: 'views/cctv/frame-id.html', controllerAs:"frameIdCtrl", resolveController: true, resolve : { user : securize(['Administrator','EunoAdministrator']) } }).
+            when('/events/:eventId/cctv/frames',     { templateUrl: 'views/cctv/frames.html',   controllerAs:"framesCtrl",  resolveController: true, resolve : { user : securize(['Administrator','EunoAdministrator']) }, menu:'cctv-frames'}).
+            when('/events/:eventId/cctv/frames/:id', { templateUrl: 'views/cctv/frame-id.html', controllerAs:"frameIdCtrl", resolveController: true, resolve : { user : securize(['Administrator','EunoAdministrator']) }, menu:'cctv-frames'}).
+
+            when('/admin/reservation/types',         { templateUrl: 'views/admin/reservation-types.html', resolveController: true, resolve : { securized : securize(['Administrator','EunoAdministrator']) }, menu:'admin'}).
+            when('/events/:eventId/cctv/feeds',      { templateUrl: 'views/cctv/feeds.html' ,   controllerAs:"feedsCtrl",   resolveController: true, resolve : { user : securize(['Administrator','EunoAdministrator']) }, menu:'admin' }).
+            when('/events/:eventId/cctv/feeds/:id',  { templateUrl: 'views/cctv/feed-id.html' , controllerAs:"feedIdCtrl",  resolveController: true, resolve : { user : securize(['Administrator','EunoAdministrator']) }, menu:'admin' }).
 
             when('/404', { templateUrl: 'views/404.html' }).
             when('/403', { templateUrl: 'views/403.html' }).
             otherwise({ redirectTo: '/404' });
-    }]);
+        }]);
 
     //============================================================
     //
@@ -38,7 +37,7 @@ define(['app', 'lodash', 'services/extended-route',  'services/authentication'],
                     var accountsUrl ='https://accounts.cbd.int';
                     var returnUrl   = $window.encodeURIComponent($window.location.href);
 
-                  $window.location.href = accountsUrl+'/signin?returnUrl=' + returnUrl; // force sign in
+                    $window.location.href = accountsUrl+'/signin?returnUrl=' + returnUrl; // force sign in
 
                     return $q(function () {});
                 }
