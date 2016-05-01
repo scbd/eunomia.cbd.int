4d78daf9b39731987b3209c817e9c7c9b28afc5d
Randy Houlahan <randy.houlahan@cbd.int>
2016-05-01 15:46:21 -0400

    update query lib on model change

--- app/directives/date-picker.js
+++ app/directives/date-picker.js
@@ -1,51 +1,78 @@
-define(['app', 'BM-date-picker'], function(app) {
+define(['app', 'moment', 'BM-date-picker'], function(app, moment) {
 
-    app.directive("datePicker", ['$timeout',function($timeout) {
+    app.directive("datePicker", ['$timeout', function($timeout) {
         return {
             restrict: 'A',
-            scope:{
-              minDate:"=?datePickerMinDate",
-              maxDate:"=?datePickerMaxDate",
-              options:"=datePicker",
-              ngModel:"="
+            require: 'ngModel',
+            scope: {
+                minDate: "=?datePickerMinDate",
+                maxDate: "=?datePickerMaxDate",
+                options: "=datePicker"
             },
-            link : function($scope, $element)
-            {
+            link: function($scope, $element, attr, ngModelCtrl) {
                 $element.bootstrapMaterialDatePicker($scope.options);
 
-                $scope.$watch('minDate',function(){
-                  if($scope.minDate)
-                    $element.bootstrapMaterialDatePicker('setMinDate',$scope.minDate);
+                //============================================================
+                //
+                //============================================================
+                $scope.$watch('minDate', function() {
+
+                    if ($scope.minDate)
+                    $scope.$applyAsync(function() {
+                        $element.bootstrapMaterialDatePicker('setMinDate', $scope.minDate);
+                      });
                 });
 
-                $scope.$watch('maxDate',function(){
-                  if($scope.maxDate)
-                    $element.bootstrapMaterialDatePicker('setMaxDate',$scope.maxDate);
+                //============================================================
+                //
+                //============================================================
+                $scope.$watch('maxDate', function() {
+                    if ($scope.maxDate)
+                        $element.bootstrapMaterialDatePicker('setMaxDate', $scope.maxDate);
+
                 });
 
-                $scope.$watch('ngModel',function(){
-                  if($scope.ngModel)
-                    dateChangeEffect();
+                //============================================================
+                //
+                //============================================================
+                $scope.$watch(function() { return ngModelCtrl.$viewValue; }, function(val, prevVal) {
+
+                    if (val && (val !== prevVal)) {
+                        dateChangeEffect();
+                        $element.bootstrapMaterialDatePicker('setDate', moment(val, $scope.options.format));
+                        $element.trigger('change');
+                    }
                 });
-                
+
+                //============================================================
+                //
+                //============================================================
+                $element.bootstrapMaterialDatePicker('change', function() {
+
+                    $scope.$applyAsync(function() {
+                        var val = $element.value();
+                        ngModelCtrl.$setViewValue(val);
+                    });
+                });
+
                 //============================================================
                 //
                 //============================================================
                 function dateChangeEffect() {
-                  var el =$element;
-                  if(el.parent().hasClass('form-group')){
-                      el.parent().addClass('is-focused');
-                      $timeout(function() {
-                        el.parent().removeClass('is-focused');
-                      }, 2000);
+                    var el = $element;
+                    if (el.parent().hasClass('form-group')) {
+                        el.parent().addClass('is-focused');
+                        $timeout(function() {
+                            el.parent().removeClass('is-focused');
+                        }, 2000);
 
-                  }else if(el.parent().parent().hasClass('form-group')){
-                    el.parent().parent().addClass('is-focused');
-                    $timeout(function() {
-                      el.parent().parent().removeClass('is-focused');
-                    }, 2000);
+                    } else if (el.parent().parent().hasClass('form-group')) {
+                        el.parent().parent().addClass('is-focused');
+                        $timeout(function() {
+                            el.parent().parent().removeClass('is-focused');
+                        }, 2000);
 
-                  }
+                    }
                 } //init
             }
         };
