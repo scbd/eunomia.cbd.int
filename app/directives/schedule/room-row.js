9f330325fb257775185f59bf95b3b9d673ebed54
Randy Houlahan <randy.houlahan@cbd.int>
2016-10-23 17:14:53 -0400

    save room on schedule bug

--- app/directives/schedule/room-row.js
+++ app/directives/schedule/room-row.js
@@ -1,10 +1,10 @@
-define(['app', 'text!./room-row.html','text!../forms/edit/room-dialog.html',
+define(['app','lodash', 'text!./room-row.html','text!../forms/edit/room-dialog.html',
   'ngDialog',
   '../forms/edit/room',
-], function(app, template,roomDialog) {
+], function(app,_, template,roomDialog) {
 
-  app.directive("roomRow", ['ngDialog',
-    function(ngDialog) {
+  app.directive("roomRow", ['ngDialog','mongoStorage','$rootScope',
+    function(ngDialog,mongoStorage,$rootScope) {
       return {
         restrict: 'E',
         template: template,
@@ -23,7 +23,7 @@ define(['app', 'text!./room-row.html','text!../forms/edit/room-dialog.html',
               if($scope.room.rowHeight)
                 $element.css('height',$scope.room.rowHeight);
             });
-            
+
             //============================================================
             //
             //============================================================
@@ -37,15 +37,39 @@ define(['app', 'text!./room-row.html','text!../forms/edit/room-dialog.html',
             //============================================================
             $scope.roomDialog = function(room) {
                 $scope.editRoom = room;
-                ngDialog.open({
+                var dialog = ngDialog.open({
                   template: roomDialog,
                   className: 'ngdialog-theme-default',
                   closeByDocument: true,
                   plain: true,
                   scope: $scope
                 });
+                dialog.closePromise.then(function(ret) {
+                      if (ret.value === 'save')  save($scope.editRoom).then(close);
+                });
             };//$scope.roomDialog
+            //============================================================
+            //
+            //============================================================
+            function save (obj){
+                return mongoStorage.save('venue-rooms', cleanDoc(obj),obj._id).then(function(){
+                            $rootScope.$broadcast("showInfo","Room '"+obj.title+"' Successfully Updated.");
+                }).catch(function(error){
+                    console.log(error);
+                    $rootScope.$broadcast("showError","There was an error saving your Room details: '"+obj.title+"' to the server.");
+                });
+            }//save
 
+            //============================================================
+            //
+            //============================================================
+            function cleanDoc(obj) {
+
+              var objClone = _.clone(obj);
+              delete(objClone.changed);
+              delete(objClone.history);
+              return objClone;
+            } //triggerChanges
           } //link
       }; //return
     }
