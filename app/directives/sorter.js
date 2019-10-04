b2cd269afa0e6758d6d19e30fbc3eb72cd06ecb4
Randy Houlahan <randy.houlahan@cbd.int>
2019-10-04 12:35:11 -0400

    remove watches

--- app/directives/sorter.js
+++ app/directives/sorter.js
@@ -4,9 +4,10 @@ define(['app'], function(app) {
   app.directive('sorter',['$timeout', function($timeout) {
   return {
    restrict: 'E',
-   scope:{binding:'=ngModel',load:'&load'},
+   require:'^ngModel',
+   scope:{binding:'=ngModel'},
    template:'<span ng-click="setSort()" class="text-nowrap" style="margin-bottom:0px;"><a ><span style="cursor:pointer;font-size:14px;color:#333;font-family: Roboto,Helvetica,Arial,sans-serif;font-weight:bold;padding: 0px 8px 0px 8px;"> {{name}}</span></a> <a ng-if="direction && isSelected()" style="cursor:pointer;"><span><i ng-if="direction===1 && isSelected()"  class="fa fa-caret-down"></i><i ng-if="direction===-1" class="fa fa-caret-up"></i></a></span></span>',
-   link: function($scope,$element,$attrs) {
+   link: function($scope,$element,$attrs, ngModelController) {
 
         $scope.name=$attrs.labelName;
         $scope.property=$attrs.property;
@@ -22,7 +23,8 @@ define(['app'], function(app) {
           $scope.direction=-$scope.direction;
           $scope.binding={};
           $scope.binding[$scope.property]=$scope.direction;
-          $scope.load();
+          ngModelController.$setViewValue($scope.binding)
+          // $scope.load();
         };
 
         //============================================================
